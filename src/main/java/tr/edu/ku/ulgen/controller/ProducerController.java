e281af6b6715794253b3a66ac8629714371bf856
Kaan Türkmen <kaanturkmen@protonmail.com>
2023-04-02 13:27:12 +0300

    Added logging to the endpoint.

--- src/main/java/tr/edu/ku/ulgen/controller/ProducerController.java
+++ src/main/java/tr/edu/ku/ulgen/controller/ProducerController.java
@@ -1,6 +1,7 @@
 package tr.edu.ku.ulgen.controller;
 
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.web.bind.annotation.*;
@@ -9,11 +10,13 @@ import tr.edu.ku.ulgen.dto.UlgenDto;
 @RestController
 @RequestMapping("/api/v1/producer")
 @AllArgsConstructor
+@Slf4j
 public class ProducerController {
     private KafkaTemplate<String, UlgenDto> kafkaTemplate;
 
     @PostMapping("/produce")
     public ResponseEntity<?> authenticate(@RequestBody UlgenDto request) {
+        log.info("Produce request is received by the producer microservice: {}", request);
         return ResponseEntity.ok(kafkaTemplate.send("ulgen", request.getUserId().toString(), request));
     }
 }
